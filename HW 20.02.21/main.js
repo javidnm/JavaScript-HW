9fd335d1d361d8b9890696aacd79ef9a4a047c7e
Javid <44782713+javidnm@users.noreply.github.com>
2021-02-20 02:06:29 +0400

    Add files via upload

---
+++ HW 20.02.21/main.js
@@ -0,0 +1,9 @@
+function homeWork(surname,gender) {
+    if (gender=="male") {
+        console.log("Mr. "+surname);
+    }
+    else{
+        console.log("Mrs. "+surname)
+    }
+}
+homeWork("Mehdibayli","male")
